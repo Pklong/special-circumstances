b998876fbfab4cfe4cda95fce9a98b3d1e1ac648
PKLong <patrickklong@gmail.com>
2017-05-29 14:27:51 -0400

    Add entry file

---
+++ asteroids.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var canvas = document.querySelector('canvas');
+  canvas.width = window.innerWidth - 25;
+  canvas.height = window.innerHeight - 25;
+
+  var ctx = canvas.getContext('2d');
+
+  var gameView = new asteroids.GameView(ctx);
+  gameView.start();
+
+})
